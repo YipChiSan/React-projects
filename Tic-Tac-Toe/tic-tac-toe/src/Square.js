1f26d6c3ce389d2ddb74db8cf8ac4bf133f9b5ed
YipChiSan <zye39@yahoo.com>
2021-12-10 13:16:40 +1100

    Finish definition of Square component

--- Tic-Tac-Toe/tic-tac-toe/src/Square.js
+++ Tic-Tac-Toe/tic-tac-toe/src/Square.js
@@ -1,2 +1,13 @@
-
 import React from "react";
+
+export default function Square(props) {
+    const myStyle = {
+        border: "solid",
+    };
+
+    return (
+        <div style={myStyle} id={props.id} occupiedBy={props.occupier}>
+
+        </div>
+        );
+}
