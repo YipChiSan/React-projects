a7fbaf8418cb699bc4a83f02d85de9d24f569e77
YipChiSan <zye39@yahoo.com>
2021-12-14 19:24:03 +1100

    Add a function to check winner

--- Tic-Tac-Toe/tic-tac-toe/src/Game.js
+++ Tic-Tac-Toe/tic-tac-toe/src/Game.js
@@ -2,6 +2,40 @@ import React from "react";
 import Board from "./Board";
 
 export default function Game() {
+
+    let board = new Array(9);
+
+    let decideWinner = function () {
+        for (let i = 0; i <= 6; i++) {
+            if (board[i] !== undefined) {
+                if (board[i] === board[i + 1] &&
+                    board[i] === board[i + 2]) {
+                    return board[i];
+                }
+
+                if (i + 6 < 9) {
+                    if (board[i] === board[i + 3] &&
+                        board[i] === board[i + 6]) {
+                        return board[i];
+                    }
+                }
+
+                if (i === 0 &&
+                    board[i] === board[4] &&
+                    board[i] === board[8]) {
+                    return board[i];
+                }
+
+                if (i === 2 &&
+                    board[i] === board[4] &&
+                    board[i] === board[6]) {
+                    return board[i];
+                }
+            }
+        }
+        return null;
+    }
+
     return (
         <Board />
     );
