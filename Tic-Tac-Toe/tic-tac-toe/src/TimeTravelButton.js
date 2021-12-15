be991256d3ed6d8417efa50ad6a99a27c5aa4901
YipChiSan <zye39@yahoo.com>
2021-12-15 15:16:16 +1100

    Finish TimeTravelButton component

--- Tic-Tac-Toe/tic-tac-toe/src/TimeTravelButton.js
+++ Tic-Tac-Toe/tic-tac-toe/src/TimeTravelButton.js
@@ -1,1 +1,10 @@
 import React from "react";
+
+export default function TimeTravelButton(props) {
+
+    const text = "Move #" + props.movdnumber;
+
+    return (
+        <button type="button" onClick={props.handleClick}>{text}</button>
+        );
+}
